815eb1b90ce05e7c39aa9813864c39c3fbda0e5b
Vitor Carvalho <vitorclocked@gmail.com>
2026-09-09 13:45:55 -0300

    feat: configura a vinculação de Views no projeto

--- app/src/main/java/com/example/fundamentosandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/fundamentosandroidapp/MainActivity.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.example.fundamentosandroidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val  navController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcvMainActivity) as? NavHostFragment
+        navHostFragment?.navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
