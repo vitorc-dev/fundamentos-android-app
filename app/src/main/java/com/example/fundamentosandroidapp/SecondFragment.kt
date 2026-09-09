a1ed660b9946856750cbaaa756c0af1ece9ab89a
Vitor Carvalho <vitorclocked@gmail.com>
2026-09-09 14:08:14 -0300

    feat: adiciona ViewBinding em SecondFragment

--- app/src/main/java/com/example/fundamentosandroidapp/SecondFragment.kt
+++ app/src/main/java/com/example/fundamentosandroidapp/SecondFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.fundamentosandroidapp.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
+    private var  _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
-
 
 }
